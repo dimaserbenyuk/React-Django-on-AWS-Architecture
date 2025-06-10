dcceafe1076eba2f026d9e924d9060e3e9b592b4
Dmitriy Serbeniuk <dserbenyukgood@gmail.com>
2025-06-10 21:29:39 +0200

    up

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -13,8 +13,8 @@ resource "aws_ecs_task_definition" "django-worker" {
   family                   = "django-worker"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "1024"
+  memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.celery_execution_role.arn
 
